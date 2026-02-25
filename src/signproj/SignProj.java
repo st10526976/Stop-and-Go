874fe3bebcf735390704f27d820e4cdfaf4155b0
hopemusasa2002-stack <hopemusasa2002@gmail.com>
2026-02-25 09:37:10 +0200

    My first submission

---
+++ src/signproj/SignProj.java
@@ -0,0 +1,22 @@
+/**
+ * The following code consist of a NO ENTRY and PASS NOW sign
+ */
+
+package signproj;
+
+/**
+ *
+ * @author ST10526976
+ */
+public class SignProj {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        System.out.println("            ---------------\n   ********\t\t   ########  \n  * \t   *\t\t # \t    # \n * \b NO ENTRY \b *\t\t# \b  PASS NOW   \b#\n  *\t   *\t\t #\t    #\n   ********\t\t   ########\n      **\t\t      ##\n      **\t\t      ##\n      **\t\t      ##\n      **\t\t     ####\n     ****\t\t    ######\n  **********\t\t ############\n|------------------------------------------------------|\n|------------------------------------------------------|");
+       
+    }
+    
+}
